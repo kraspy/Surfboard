a8b4f2269a4dfb3e6f004133e336a362c426ef29
Evgeniy Doroshenko <kraspy@ynandex.ru>
2024-02-13 16:10:36 +0700

    Week4: Dropdown and Slider added

--- js/main.js
+++ js/main.js
@@ -29,3 +29,41 @@ function toggleMenu(element) {
 }
 
 hamburger.addEventListener('click',  toggleMenu);
+
+
+// Team section
+const teamTitles = document.querySelectorAll('.team__title');
+
+teamTitles.forEach((element) => {
+  element.addEventListener('click', () => {
+    element.classList.toggle('team__title--active');
+  })
+})
+
+
+// Owl carousel
+$(document).ready(function(){
+  var owl = $(".owl-carousel").owlCarousel({
+    'items': 1,
+    'mouseDrag': false,
+    'dots': true,
+    'dotsEach': true,
+    'dotsContainer': '.review-selector__list',
+  });
+
+  owl.on('changed.owl.carousel', function(event) {
+    var currentItem = event.item.index; // Получаем индекс текущего активного слайда
+
+    $('.review-selector__item').removeClass('review-selector__item--active');
+
+    $('.review-selector__list .review-selector__item').eq(currentItem).addClass('review-selector__item--active');
+  });
+
+  $('.review-selector__link').click(function(e) {
+    e.preventDefault();
+    e.target.classList.toggle('team__title--active');
+    var index = $(this).closest('.review-selector__item').index();
+    owl.trigger('to.owl.carousel', [index, 300]);
+  });
+});
+
